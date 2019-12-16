decaf1eeaead88b09abb2b062b0fc0834d075de8
tdonovan79 <tdonovan79@gmail.com>
2019-12-15 23:25:39 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,26 @@
 class Instructor
-
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    #if there is a test with name and student, change status to passed else create new instance
+    def pass_student(student, search_name)
+        if BoatingTest.all.find{|test| test.student == student && test.test_name == search_name}
+            BoatingTest.all.find{|test| test.student == student && test.test_name == search_name}.status = "passed"
+        else
+            BoatingTest.new(student, search_name, "passed", self)
+        end
+    end
+    def fail_student(student, search_name)
+        if BoatingTest.all.find{|test| test.student == student && test.test_name == search_name}
+            BoatingTest.all.find{|test| test.student == student && test.test_name == search_name}.status = "failed"
+        else
+            BoatingTest.new(student, search_name, "failed", self)
+        end
+    end
 end
